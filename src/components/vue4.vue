779cf0efe5d1e8c2d0f3f34063e6080630dded35
Luisangonzalez <luisangonzalez@gmail.com>
2017-06-13 00:19:32 +0200

    new element in route

---
+++ src/components/vue4.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+  <div id="vue4">
+    <ol>
+      <li v-for="todo in todos">
+        {{ todo.text }}
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vue4',
+  data() {
+    return {
+      todos: [
+        { text: 'Learn JavaScript' },
+        { text: 'Learn Vue' },
+        { text: 'Build something awesome' },
+      ],
+    };
+  },
+  created: () => {
+     // eslint-disable-next-line
+    console.log('console');  // eslint-disable-next-line
+  },
+  methods: {
+    log: () => {
+       // eslint-disable-next-line
+      console.log('console');  // eslint-disable-next-line
+    },
+  },
+};
+</script>
+
+<style lang="css">
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Hello from '@/components/Hello';
+import vue4 from '@/components/vue4';
+
 
 Vue.use(Router);
 
@@ -10,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello,
+    },
+    {
+      path: '/vue4',
+      name: 'vue4',
+      component: vue4,
     },
   ],
 });
